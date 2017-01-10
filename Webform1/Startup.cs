3e03daf7fa80bedb697a24f25429f9dbc3cc3648
binsina <binsina9@gmail.com>
2017-01-10 15:12:28 -0500

    Add project files.

---
+++ Webform1/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Webform1.Startup))]
+namespace Webform1
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
